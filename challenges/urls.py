fed3fbdc7b01c2f3036dedc320452002e61da93c
Strahinja <strahinja.vuckovic94@gmail.com>
2023-02-28 13:39:03 +0100

    Add html data to response

--- challenges/urls.py
+++ challenges/urls.py
@@ -5,5 +5,5 @@ urlpatterns = [
     # path("january", views.january),
     # path("february", views.february),
     path("<int:month>", views.monthly_challenge_by_number),
-    path("<str:month>", views.monthly_challenge)
+    path("<str:month>", views.monthly_challenge, name="month-challenge")
 ]
--- challenges/views.py
+++ challenges/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseNotFound, HttpResponseRedirect
+from django.urls import reverse
 
 monthly_challenges = {
     "january": "Text January",
@@ -29,12 +30,14 @@ def monthly_challenge_by_number(request, month):
         return HttpResponseNotFound("Invalid month!!!")
     
     redirect_month = months[month - 1]
-    return HttpResponseRedirect("/challenges/" + redirect_month)
+    redirect_path = reverse("month-challenge", args = [redirect_month])
+    return HttpResponseRedirect(redirect_path)
 
 def monthly_challenge(request, month):
     try:
         challenge_text = monthly_challenges[month]
-        return HttpResponse(challenge_text)
+        response_data = f"<h1>{challenge_text}</h1>"
+        return HttpResponse(response_data)
     except:
         return HttpResponseNotFound("This month is not supported jet!!!")
     
