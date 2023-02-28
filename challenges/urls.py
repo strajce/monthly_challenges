3e5dea7718b2a5a1ecfa1e7d4436f0b1bdd564ac
Strahinja <strahinja.vuckovic94@gmail.com>
2023-02-28 10:56:44 +0100

    Add dinamyc path and handling not found feature

--- challenges/urls.py
+++ challenges/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("january", views.january),
-    path("february", views.february),
+    # path("january", views.january),
+    # path("february", views.february),
+    path("<month>", views.monthly_challenge)
 ]
--- challenges/views.py
+++ challenges/views.py
@@ -1,10 +1,20 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseNotFound
 
 # Create your views here.
 
-def index(request):
-    return HttpResponse("This Works!!!")
+# def january(request):
+#     return HttpResponse("This Works!!!")
 
-def february(request):
-    return HttpResponse("February")
+# def february(request):
+#     return HttpResponse("February")
+
+def monthly_challenge(request, month):
+    challenge_text = None
+    if (month == "january"):
+        challenge_text = "This Works for January"
+    elif (month == "february"):
+        challenge_text = "Learn django"
+    else:
+        return HttpResponseNotFound("Month is not currently supported!!!")
+    return HttpResponse(challenge_text)
