e24684e3a6576b11004604880888fa3a03574d68
Strahinja <strahinja.vuckovic94@gmail.com>
2023-02-28 13:50:29 +0100

    Implement basic challenge route page

--- challenges/urls.py
+++ challenges/urls.py
@@ -4,6 +4,7 @@ from . import views
 urlpatterns = [
     # path("january", views.january),
     # path("february", views.february),
+    path("", views.index),
     path("<int:month>", views.monthly_challenge_by_number),
     path("<str:month>", views.monthly_challenge, name="month-challenge")
 ]
