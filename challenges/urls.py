e94eb5ce5c6f8f8c74e3176be0220d0838eff8be
Strahinja <strahinja.vuckovic94@gmail.com>
2023-02-28 11:25:38 +0100

    Add if else

--- challenges/urls.py
+++ challenges/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
     # path("january", views.january),
     # path("february", views.february),
-    path("<month>", views.monthly_challenge)
+    path("<int:month>", views.monthly_challenge_by_number),
+    path("<str:month>", views.monthly_challenge)
 ]
--- challenges/views.py
+++ challenges/views.py
@@ -1,6 +1,21 @@
 from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseNotFound
 
+monthly_challenges = {
+    "january": "Text January",
+    "february": "Text February",
+    "march": "Text March",
+    "april": "Text April",
+    "may": "Text May",
+    "june": "Text June",
+    "july": "Text July",
+    "august": "Text August",
+    "september": "Text September",
+    "october": "Text October",
+    "november": "Text November",
+    "december": "Text December"
+}
+
 # Create your views here.
 
 # def january(request):
@@ -8,6 +23,8 @@ from django.http import HttpResponse, HttpResponseNotFound
 
 # def february(request):
 #     return HttpResponse("February")
+def monthly_challenge_by_number(request, month):
+    return HttpResponse(month)
 
 def monthly_challenge(request, month):
     challenge_text = None
