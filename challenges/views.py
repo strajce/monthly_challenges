990d7c33fde1ebf811900045338f46191b6930cc
Strahinja <strahinja.vuckovic94@gmail.com>
2023-03-01 09:41:11 +0100

    Implement challenge.html template file and setup path in settings.py

--- challenges/views.py
+++ challenges/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseNotFound, HttpResponseRedirect
 from django.urls import reverse
+from django.template.loader import render_to_string
 
 monthly_challenges = {
     "january": "Text January",
@@ -16,14 +17,6 @@ monthly_challenges = {
     "november": "Text November",
     "december": "Text December"
 }
-
-# Create your views here.
-
-# def january(request):
-#     return HttpResponse("This Works!!!")
-
-# def february(request):
-#     return HttpResponse("February")
 
 def index(request):
     list_items = ""
@@ -50,7 +43,7 @@ def monthly_challenge_by_number(request, month):
 def monthly_challenge(request, month):
     try:
         challenge_text = monthly_challenges[month]
-        response_data = f"<h1>{challenge_text}</h1>"
+        response_data = render_to_string("challenges/challenge.html")
         return HttpResponse(response_data)
     except:
         return HttpResponseNotFound("This month is not supported jet!!!")
