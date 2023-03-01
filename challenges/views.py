4fc77ca8b8deae98dcc976032aa2810a3e932a4c
Strahinja <strahinja.vuckovic94@gmail.com>
2023-03-01 11:16:54 +0100

    Adding 404 template

--- challenges/views.py
+++ challenges/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.http import HttpResponse, HttpResponseNotFound, HttpResponseRedirect
+from django.http import Http404, HttpResponseNotFound, HttpResponseRedirect
 from django.urls import reverse
 
 monthly_challenges = {
