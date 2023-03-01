0b0f4054a8bbf57edfa78aeee3db0238762abd4e
Strahinja <strahinja.vuckovic94@gmail.com>
2023-03-01 10:26:48 +0100

    Intradusing tags in templates

--- challenges/views.py
+++ challenges/views.py
@@ -1,7 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseNotFound, HttpResponseRedirect
 from django.urls import reverse
-from django.template.loader import render_to_string
 
 monthly_challenges = {
     "january": "Text January",
@@ -21,15 +20,19 @@ monthly_challenges = {
 def index(request):
     list_items = ""
     months = list(monthly_challenges.keys())
+    
+    return render(request, "challenges/index.html", {
+        "months" : months,
+    })
 
-    for month in months:
-        capitalized_month = month.capitalize()
-        month_path = reverse("month-challenge", args=[month])
-        list_items += f"<li><a href=\"{month_path}\">{capitalized_month}</a></li>"
+    # for month in months:
+    #     capitalized_month = month.capitalize()
+    #     month_path = reverse("month-challenge", args=[month])
+    #     list_items += f"<li><a href=\"{month_path}\">{capitalized_month}</a></li>"
         
-    response_data = f"<ul>{list_items}</ul>"
+    # response_data = f"<ul>{list_items}</ul>"
 
-    return HttpResponse(response_data)
+    # return HttpResponse(response_data)
 
 def monthly_challenge_by_number(request, month):
     months = list(monthly_challenges.keys())
@@ -43,8 +46,10 @@ def monthly_challenge_by_number(request, month):
 def monthly_challenge(request, month):
     try:
         challenge_text = monthly_challenges[month]
-        response_data = render_to_string("challenges/challenge.html")
-        return HttpResponse(response_data)
+        return render(request, "challenges/challenge.html", {
+            "text" : challenge_text,
+            "month_name" : month,
+        })
     except:
         return HttpResponseNotFound("This month is not supported jet!!!")
     
